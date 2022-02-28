b02adf8ecb8667efababf8e395969e7edf3c844f
Estebrock09 <estebanquevedo.p@gmail.com>
2022-02-28 18:04:55 -0500

    [FIX] test.py

--- dags/bigquery_insert.py
+++ dags/bigquery_insert.py
@@ -3,37 +3,38 @@ from airflow.operators.dummy import DummyOperator
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 import datetime
 
-nameDAG = 'airflow_bigquery_insert'
-project = 'hallowed-hold-337921'
-owner = 'estebrocktest'
-email = ['estebrocktest@gmail.com']
-GBQ_CONNECTION_ID = 'bigquery_default'
+nameDAG = "airflow_bigquery_insert"
+project = "hallowed-hold-337921"
+owner = "estebrocktest"
+email = ["estebrocktest@gmail.com"]
+GBQ_CONNECTION_ID = "bigquery_default"
 
 default_args = {
-    'owner': owner,
-    'depends_on_past': False,
-    'start_date': datetime.datetime(2022, 1, 19),
-    'email': email,
-    'email_on_failure': True,
-    'email_on_retry': True,
-    'retries': 3,
-    'retry_delay': datetime.timedelta(minutes=1),
-    'project_id': project,
+    "owner": owner,
+    "depends_on_past": False,
+    "start_date": datetime.datetime(2022, 1, 19),
+    "email": email,
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 3,
+    "retry_delay": datetime.timedelta(minutes=1),
+    "project_id": project,
 }
 
-query_bq_op = '''
+query_bq_op = """
 INSERT `{{ params.google_project_id }}.{{ params.queryDataset }}.{{ params.queryTable}}` (
     date
 )
 SELECT
     CURRENT_TIMESTAMP() as date
-'''
+"""
 
-with DAG(nameDAG,
-         default_args=default_args,
-         catchup=False,
-         max_active_runs=3,
-         schedule_interval="30 * * * *") as dag:
+with DAG(
+    nameDAG,
+    default_args=default_args,
+    catchup=False,
+    max_active_runs=3,
+    schedule_interval="30 * * * *") as dag:
 
     t_begin = DummyOperator(task_id="begin")
 
